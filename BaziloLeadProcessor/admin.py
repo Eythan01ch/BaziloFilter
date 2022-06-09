96919c274b5db11a8366512ce7bc344ce3de80b5
cyber237 <58531874+cyber237@users.noreply.github.com>
2022-06-09 13:10:24 +0100

    Update admin.py

--- BaziloLeadProcessor/admin.py
+++ BaziloLeadProcessor/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.sites.register([LeadEmailInfo,LeadPhoneInfo,Lead,APIUtils,InvalidName])
+admin.site.register([LeadEmailInfo,LeadPhoneInfo,Lead,APIUtils,InvalidName])
